cac64a2001d4a686453ef307a3be1a0a74a9c281
abner-castro <abner.castro@wizeline.com>
2022-04-28 13:22:57 -0500

    removed unused code

--- MiniBootcamp/Extensions/UIViewExtensions.swift
+++ MiniBootcamp/Extensions/UIViewExtensions.swift
@@ -10,29 +10,6 @@ import UIKit
 
 extension UIView {
     
-    func dimensionAnchors(height: NSLayoutDimension?, heightMultiplier: CGFloat = 1.0, width: NSLayoutDimension?, widthMultiplier: CGFloat = 1.0) {
-        translatesAutoresizingMaskIntoConstraints = false
-        if let height = height {
-            heightAnchor.constraint(equalTo: height, multiplier: heightMultiplier).isActive = true
-        }
-        
-        if let width = width {
-            widthAnchor.constraint(equalTo: width, multiplier: widthMultiplier).isActive = true
-        }
-    }
-    
-    func centerAnchors(centerX: NSLayoutXAxisAnchor?, centerY: NSLayoutYAxisAnchor?) {
-        translatesAutoresizingMaskIntoConstraints = false
-        
-        if let centerX = centerX {
-            centerXAnchor.constraint(equalTo: centerX).isActive = true
-        }
-        
-        if let centerY = centerY {
-            centerYAnchor.constraint(equalTo: centerY).isActive = true
-        }
-    }
-    
     func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, trailing: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero, multiplier: CGFloat = 0.0) {
         
         translatesAutoresizingMaskIntoConstraints = false
