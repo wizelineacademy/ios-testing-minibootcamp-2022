384a393a23a2fb21b7a66e946cb7d9d69e691da8
abner-castro <abner.castro@wizeline.com>
2022-06-21 17:19:59 -0500

    ui creation with TDD

--- MiniBootcamp/Modules/Feed/FeedViewController.swift
+++ MiniBootcamp/Modules/Feed/FeedViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let tableView: UITableView = create {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        
+        tableView.anchor(top: view.topAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, bottom: view.bottomAnchor)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        TweetCell()
     }
 
 }
