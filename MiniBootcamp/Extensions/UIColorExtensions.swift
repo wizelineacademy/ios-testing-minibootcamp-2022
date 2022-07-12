4790ff6f36c15b932ff638c348b269f821812b2d
Juan David Torres Velasco <juandatorr_1999@hotmail.com>
2022-07-11 20:57:04 -0500

    Tests

--- MiniBootcamp/Extensions/UIColorExtensions.swift
+++ MiniBootcamp/Extensions/UIColorExtensions.swift
@@ -11,3 +11,4 @@ import UIKit
 extension UIColor {
     static let redMain = UIColor(red: 0.94, green: 0.31, blue: 0.31, alpha: 1.00)
 }
+
--- MiniBootcampTests/Composition Tests/iOSViewControllerFactoryTests.swift
+++ MiniBootcampTests/Composition Tests/iOSViewControllerFactoryTests.swift
@@ -9,6 +9,9 @@ import XCTest
 @testable import MiniBootcamp
 
 class iOSViewControllerFactoryTests: XCTestCase {
+    var sut_tweetvm: TweetViewModel?
+    var sut_uservm: UserViewModel?
+    lazy var bundle = Bundle(for: self.classForCoder)
     
     func test_feedViewController_isFeedViewControllerType() {
         
@@ -16,5 +19,22 @@ class iOSViewControllerFactoryTests: XCTestCase {
         let feedVC = sut.feedViewController() as? FeedViewController
         XCTAssertNotNil(feedVC)
         
+    }
+    
+    func test_DetailViewController_isDetailViewControllerType() {
+        
+        let sut = iOSViewControllerFactory()
+        let feedVC = sut.detailViewController(tweetViewModel:sut_tweetvm!, userViewModel: sut_uservm!, userImage: UIImage(named:"gato")!)
+        XCTAssertNotNil(feedVC)
+        
+    }
+    
+    override func setUpWithError() throws {
+        let data = try Data.from(filename: "tweetFake", in: bundle, ofType: "json")!
+        let data2 = try Data.from(filename: "userFake", in: bundle, ofType: "json")!
+        let tweet: Tweet! = try JSONDecoder().decode(Tweet.self, from: data)
+        let user: User! = try JSONDecoder().decode(User.self, from: data2)
+        self.sut_tweetvm = TweetViewModel(tweet)
+        self.sut_uservm = UserViewModel(user)
     }
 }
